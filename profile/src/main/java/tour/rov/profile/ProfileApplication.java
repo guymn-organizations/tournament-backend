a50ebfc5dd70ccbf35d5866a1d65cbf0fbedf4f7
guymn <117151853+guymn@users.noreply.github.com>
2023-10-19 11:57:04 +0700

    use mongoDB

--- profile/src/main/java/tour/rov/profile/ProfileApplication.java
+++ profile/src/main/java/tour/rov/profile/ProfileApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TourProfileApplication {
+public class ProfileApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TourProfileApplication.class, args);
+		SpringApplication.run(ProfileApplication.class, args);
 	}
 
 }
--- profile/src/test/java/tour/rov/profile/ProfileApplicationTests.java
+++ profile/src/test/java/tour/rov/profile/ProfileApplicationTests.java
@@ -1,10 +1,10 @@
-package tour.rov.leagues;
+package tour.rov.profile;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LeaguesApplicationTests {
+class ProfileApplicationTests {
 
 	@Test
 	void contextLoads() {
