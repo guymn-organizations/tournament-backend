dc5d13f6fc7c97a12e89a7026468c9d9bc37d6bb
guymn <117151853+guymn@users.noreply.github.com>
2023-10-25 12:12:11 +0700

    player post controller

--- profile/src/main/java/tour/rov/profile/controller/PlayerPostController.java
+++ profile/src/main/java/tour/rov/profile/controller/PlayerPostController.java
@@ -1,14 +1,21 @@
 package tour.rov.profile.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import tour.rov.profile.model.PlayerPost;
+import tour.rov.profile.model.Position;
 import tour.rov.profile.service.PlayerPostService;
 
 @RestController
@@ -20,6 +27,7 @@ public class PlayerPostController {
     @PostMapping("/create")
     public ResponseEntity<?> createPost(@RequestBody PlayerPost playerPost) {
         try {
+            playerPost.setPositions(new ArrayList<Position>());
             playerPostService.savePlayerPost(playerPost);
             return ResponseEntity.status(HttpStatus.CREATED).body("Profile was created\n" + playerPost);
         } catch (Exception e) {
@@ -27,4 +35,27 @@ public class PlayerPostController {
                     .body("Failed to create post : " + e.getMessage());
         }
     }
+
+    @GetMapping
+    public ResponseEntity<?> getAllPlayerPost() {
+        try {
+            List<PlayerPost> playerPosts = playerPostService.getAllPlayerPosts();
+            return ResponseEntity.ok(playerPosts);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to get posts : " + e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editPost(@PathVariable String id, @RequestBody PlayerPost playerPost) {
+        try {
+            playerPostService.updatePlayerPost(id, playerPost);
+            return ResponseEntity.ok().body("Post was updated");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to get posts : " + e.getMessage());
+        }
+    }
+
 }
--- profile/src/main/java/tour/rov/profile/service/PlayerPostService.java
+++ profile/src/main/java/tour/rov/profile/service/PlayerPostService.java
@@ -1,5 +1,7 @@
 package tour.rov.profile.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,26 @@ public class PlayerPostService {
 
     public void savePlayerPost(PlayerPost playerPost) {
         playerPostRepository.save(playerPost);
+    }
+
+    public List<PlayerPost> getAllPlayerPosts() {
+        return playerPostRepository.findAll();
+    }
+
+    public PlayerPost findById(String id) {
+        return playerPostRepository.findById(id).get();
+    }
+
+    public void updatePlayerPost(String id, PlayerPost updatedPlayerPost) {
+        PlayerPost playerPost = findById(id);
+
+        if (updatedPlayerPost.getPositions() != null) {
+            playerPost.setPositions(updatedPlayerPost.getPositions());
+        }
+        if (updatedPlayerPost.getProfile() != null) {
+            playerPost.setProfile(updatedPlayerPost.getProfile());
+        }
+
+        savePlayerPost(playerPost);
     }
 }
