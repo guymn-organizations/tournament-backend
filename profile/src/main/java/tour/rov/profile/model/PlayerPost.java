fccd10b86fc9cd5eac2a3d58109da7229823768e
guymn <117151853+guymn@users.noreply.github.com>
2023-10-25 11:52:15 +0700

    add @id

--- profile/src/main/java/tour/rov/profile/model/PlayerPost.java
+++ profile/src/main/java/tour/rov/profile/model/PlayerPost.java
@@ -2,21 +2,35 @@ package tour.rov.profile.model;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "player_post")
 public class PlayerPost {
+    @Id
+    private String id;
+
     @DBRef
     private Profile profile;
+
     private List<Position> positions;
 
     public PlayerPost() {
     }
 
-    public PlayerPost(Profile profile, List<Position> positions) {
+    public PlayerPost(String id, Profile profile, List<Position> positions) {
+        this.id = id;
         this.profile = profile;
         this.positions = positions;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public Profile getProfile() {
--- profile/src/main/java/tour/rov/profile/model/TeamPost.java
+++ profile/src/main/java/tour/rov/profile/model/TeamPost.java
@@ -2,21 +2,35 @@ package tour.rov.profile.model;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "team_post")
 public class TeamPost {
+    @Id
+    private String id;
+
     @DBRef
     private Team team;
+
     private List<Position> positions;
 
     public TeamPost() {
     }
 
-    public TeamPost(Team team, List<Position> positions) {
+    public TeamPost(String id, Team team, List<Position> positions) {
+        this.id = id;
         this.team = team;
         this.positions = positions;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public Team getTeam() {
