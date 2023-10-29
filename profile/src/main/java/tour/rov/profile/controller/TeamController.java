7f084933667255763ebfe48c16a9d19958d2117f
guymn <117151853+guymn@users.noreply.github.com>
2023-10-29 21:28:57 +0700

    CrossOrigin

--- profile/src/main/java/tour/rov/profile/controller/TeamController.java
+++ profile/src/main/java/tour/rov/profile/controller/TeamController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import tour.rov.profile.service.TeamService;
 
 @RestController
 @RequestMapping("/teams")
+@CrossOrigin(origins = { "http://localhost:4200/" })
 public class TeamController {
 
     @Autowired
