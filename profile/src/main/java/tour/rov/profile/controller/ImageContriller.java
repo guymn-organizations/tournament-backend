aadaa2296bcc96951ddb5fcec71b6a4ffc749f38
guymn <117151853+guymn@users.noreply.github.com>
2023-10-30 16:07:05 +0700

    image

--- profile/src/main/java/tour/rov/profile/controller/ImageContriller.java
+++ profile/src/main/java/tour/rov/profile/controller/ImageContriller.java
@@ -1,7 +1,5 @@
 package tour.rov.profile.controller;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,13 +7,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import tour.rov.profile.model.Image;
-import tour.rov.profile.model.PlayerPost;
-import tour.rov.profile.model.Position;
 import tour.rov.profile.service.ImageService;
 
 @RestController
