a658287bb668eccc36dc2ffa08140b92209d7255
dagon1234 <dagontunk1234@gmail.com>
2023-11-01 22:28:43 +0700

    createAdvert

--- profile/src/main/java/tour/rov/profile/service/AdvertService.java
+++ profile/src/main/java/tour/rov/profile/service/AdvertService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import tour.rov.profile.model.Advert;
+import tour.rov.profile.model.Image;
 import tour.rov.profile.repository.AdvertRepo;
 
 @Service
@@ -13,11 +14,23 @@ public class AdvertService {
     @Autowired
     private AdvertRepo advertRepo;
 
+    @Autowired
+    private ImageService imageService;
+
     public void saveAdvert(Advert advert) {
         advertRepo.save(advert);
     }
 
     public List<Advert> getAllAdverts() {
         return advertRepo.findAll();
+    }
+
+    public void createAdvert(Advert advert){
+        Image image = new Image();
+        image.setImageUrl(advert.getImageAdvertUrl());
+        imageService.saveImage(image);
+
+        advert.setImageAdvertUrl(image.getId());
+        saveAdvert(advert);
     }
 }
