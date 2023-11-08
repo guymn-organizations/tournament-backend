3297cb90830e10384b4cf78617bf065155a1233a
guymn <117151853+guymn@users.noreply.github.com>
2023-11-09 00:42:12 +0700

    merage sup

--- profile/src/main/java/tour/rov/profile/model/Tournament.java
+++ profile/src/main/java/tour/rov/profile/model/Tournament.java
@@ -62,7 +62,7 @@ public class Tournament {
     
     public Tournament() {
         this.teamJoin = new ArrayList<>();
-        this.status = status.รอดำเนินการ;
+        this.status = Status.รอดำเนินการ;
     }
 
     public Tournament(String name, String detail, Double reward, LocalDate startRegisterDate,
