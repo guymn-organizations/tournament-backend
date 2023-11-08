aad8369e0d412d58d8b0f49f3486a36b7d39a7b9
dagon1234 <dagontunk1234@gmail.com>
2023-11-08 21:34:37 +0700

    advert test

--- profile/src/test/java/tour/rov/profile/controller/AdvertControllerTest.java
+++ profile/src/test/java/tour/rov/profile/controller/AdvertControllerTest.java
@@ -1,26 +1,56 @@
 package tour.rov.profile.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import tour.rov.profile.model.Advert;
+import tour.rov.profile.service.AdvertService;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.List;
+
+@WebMvcTest(AdvertController.class)
 public class AdvertControllerTest {
 
     @Autowired
-    private AdvertController advertController;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AdvertService advertService;
 
     @Test
-    public void testCreateAdvert() {
-        Advert advert = new Advert(); // Create an Advert object with test data
+    public void testGetAllAdvert() throws Exception {
+        // Arrange
+        List<Advert> adverts = new ArrayList<>();
+        adverts.add(new Advert("image1.jpg", "link1"));
+        adverts.add(new Advert("image2.jpg", "link2"));
 
-        ResponseEntity<?> response = advertController.createAdvert(advert);
+        Mockito.when(advertService.getAllAdverts()).thenReturn(adverts);
 
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/advert")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].imageAdvertUrl").value("image1.jpg"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].imageAdvertUrl").value("image2.jpg"));
+    }
+
+    @Test
+    public void testCreateAdvert() throws Exception {
+        // Arrange
+        Advert newAdvert = new Advert("newImage.jpg", "newLink");
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/advert/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"imageAdvertUrl\": \"newImage.jpg\", \"linkAdvertUrl\": \"newLink\"}"))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().string("Advertisement was created\n" + newAdvert));
     }
 }
